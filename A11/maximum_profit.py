e66762e171813c490111df651d7a475e566e1fdc
makutexas <115372240+makutexas@users.noreply.github.com>
2022-11-26 17:44:55 -0600

    Update maximum_profit.py

--- A11/maximum_profit.py
+++ A11/maximum_profit.py
@@ -41,13 +41,11 @@ def main():
     line = line.strip()
     money = int(line)
 
-
 # The second line includes an integer number which is the number of houses listed for sale.
     line = sys.stdin.readline()
     line = line.strip()
     num_houses = int(line)
 
-    
     # The third line is a list of house prices in million dollar which is a list of \textit{integer numbers} (Consider that house prices can be an integer number in million dollar only).
     line = sys.stdin.readline()
     line = line.strip()
@@ -55,8 +53,6 @@ def main():
     for i in range(0, len(prices)):
         prices[i] = int(prices[i])
     
-   
-
     # read the number of vertices
     line = sys.stdin.readline()
     line = line.strip()
@@ -70,8 +66,5 @@ def main():
 # Add your functions and call them to generate the result. 
 
     print("%.2f" % result)
-
-    
-
     
 main()
